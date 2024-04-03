7bee17c3538bc51c31ce791e7df826747aa463f4
Alfred-Nio <nioalfred@gmail.com>
2024-04-03 10:45:57 +0700

    first commit

---
+++ js/script.js
@@ -0,0 +1,41 @@
+// var angka = 1; // menggunakan var tidak disarankan
+// let angka = 1; // menggungkan let (bisa diganti variabel)
+// angka = 2;
+// const angkaSatuVariabel = 1; // menggunakan const
+// let text = "Hallo JavaScript";
+// let lima = 5; // Tipe data Number
+// let dua = 2;
+// let perkalian = lima * dua;
+// let hasil = perkalian;
+// let sepuluh = 10;
+// let tiga = 3;
+
+
+// let tambah = 20+20;
+// let kurang= 40-20;
+// let bagi = 20/10;
+// let kali = 20*2;
+// console.log("Hasil Operasi Matematika :");
+// console.log("Hasil Pengurangan = ", kurang);
+// console.log("Hasil Penambahan = ", tambah);
+// console.log("Hasil Pembagian = ", bagi);
+// console.log("Hasil Perkalian = ", kali);
+
+// CONCATENATION (Pennggabungan antara variabel)
+// let kakBaktiar = "Baktiar";
+// let kakTyan = "Tyan";
+// let namaLengkap = kakBaktiar + " Spasi "+ kakTyan;
+
+let jumlah = 20 + 20;
+let kurang = 20 - 10;
+let kali = 20 * 2;
+let bagi = 20 / 10;
+let hasilTambah = "Hasil perhitungan = " + jumlah;
+let hasilKurang = "Hasil perhitungan = " + kurang;
+let hasilKali = "Hasil perhitungan = " + kali;
+let hasilBagi = "Hasil perhitungan = " + bagi;
+
+console.log(hasilTambah);
+console.log(hasilKali);
+console.log(hasilKali);
+console.log(hasilBagi);
